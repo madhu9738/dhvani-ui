705f3513c259b6dce0365dd9e4b0e69801dbdf38
Ubuntu <ubuntu@ip-172-31-76-79.ec2.internal>
2025-05-24 18:13:53 +0000

    Point to backend via ngrok

--- public/mic-processor.js
+++ public/mic-processor.js
@@ -1,15 +1,20 @@
-
 class MicProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
-    this.buffer = [];
+    this.frame = [];
   }
 
   process(inputs, outputs, parameters) {
     const input = inputs[0];
     if (input && input[0]) {
-      const pcm = input[0].map(sample => Math.max(-1, Math.min(1, sample)) * 32767);
-      this.port.postMessage(Int16Array.from(pcm));
+      for (let i = 0; i < input[0].length; i++) {
+        const sample = Math.max(-1, Math.min(1, input[0][i])) * 32767;
+        this.frame.push(sample);
+        if (this.frame.length === 480) {
+          this.port.postMessage(Int16Array.from(this.frame));
+          this.frame = [];
+        }
+      }
     }
     return true;
   }
